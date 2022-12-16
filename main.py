db950db6edbff15d2faa3b10c0978097698a4069
PetrikPavel <academ7799@gmail.com>
2022-12-16 11:14:59 +0300

    Add python_22 16.12.22

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm in python_2!')
     print_hi('PyCharm. Version 2!')
+    print_hi('PyCharm. Version 3!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
